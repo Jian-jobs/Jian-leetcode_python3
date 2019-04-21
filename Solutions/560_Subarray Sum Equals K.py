2afc8fa8abc8345224eec2fd8819278036d72ea8
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-04-21 19:21:58 +1000

    Type：hash table similar: 1 Two Sum

--- Solutions/560_Subarray Sum Equals K.py
+++ Solutions/560_Subarray Sum Equals K.py
@@ -13,5 +13,14 @@ The length of the array is in range [1, 20,000].
 The range of numbers in the array is [-1000, 1000] and
 the range of the integer k is [-1e7, 1e7].
 
-
 '''
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        '''
+
+        :param nums:
+        :param k:
+        :return:
+        '''
