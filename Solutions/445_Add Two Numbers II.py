8baa0be0a6a6cf1bd8c7c247e6c8a6a5b778a930
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-04-24 13:18:36 +1000

    similar: 002 Add two Numbers

---
+++ Solutions/445_Add Two Numbers II.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        '''
+
+        :param l1: ListNode
+        :param l2: ListNode
+        :return: ListNode
+        '''
+
