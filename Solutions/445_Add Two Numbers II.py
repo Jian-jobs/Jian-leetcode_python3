034f695239139a338e9458d322fac7fb8cdc4e35
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-04-24 15:38:12 +1000

    similar: 002 Add two Numbers use: list.pop()

--- Solutions/445_Add Two Numbers II.py
+++ Solutions/445_Add Two Numbers II.py
@@ -29,7 +29,85 @@ class Solution:
         :param l2: ListNode
         :return: ListNode
         '''
+        stack1 = []
+        stack2 = []
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
+        answer = None
+        carry = 0
+        while stack1 and stack2:
+            add = stack1.pop()+stack2.pop()+carry
+            carry = add // 10
+            temp = answer
+            answer = ListNode(add % 10)
+            answer.next = temp
+        l = stack1 if stack1 else stack2
+        while l:
+            add = l.pop()+carry
+            carry = add // 10
+            temp = answer
+            answer = ListNode(add % 10)
+            answer.next = temp
+        if carry:
+            temp = answer
+            answer = ListNode(1)
+            answer.next = temp
+        return answer
 
-        carry += (l1.val if l1 else None) + (l2.val if l2 else None)
+l1 = ListNode(7)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+l1.next.next.next = ListNode(3)
 
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+print(Solution().addTwoNumbers(l1, l2))
 
+'''
+sumbitted answer;
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        stack1 = []
+        stack2 = []
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next       
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
+        answer = None
+        carry = 0
+        while stack1 and stack2:
+            addup = stack1.pop()+stack2.pop()+carry
+            carry = addup // 10
+            temp = answer
+            answer = ListNode(addup % 10)
+            answer.next = temp
+            
+        l = stack1 if stack1 else stack2
+        while l:
+            addup = l.pop()+carry
+            carry = addup // 10
+            temp = answer
+            answer = ListNode(addup % 10)
+            answer.next = temp
+            
+        if carry:
+            temp = answer
+            answer = ListNode(1)
+            answer.next = temp
+           
+        return answer
+'''
