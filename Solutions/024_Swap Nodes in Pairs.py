3eb475094ca5de3554e7ea8deac8604fd8610fbb
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-04-26 13:43:52 +1000

    two ways: iteration and recursion

--- Solutions/024_Swap Nodes in Pairs.py
+++ Solutions/024_Swap Nodes in Pairs.py
@@ -14,10 +14,10 @@ Given 1->2->3->4, you should return the list as 2->1->4->3.
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def swapPairs(self, head):
@@ -26,3 +26,17 @@ class Solution:
         :param head: ListNode
         :return: ListNode
         '''
+        dummy = ListNode(0)
+        dummy.next = head
+        res = dummy
+
+        while res.next and res.next.next:
+            first = res.next
+            second = res.next.next
+            res.next = second
+            first.next = second.next
+            second.next = first
+            res = res.next.next
+
+        return dummy.next
+
