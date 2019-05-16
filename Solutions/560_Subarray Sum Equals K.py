e49f59a4b3a9e60ecb1eafa1e80bfe0a2fe3f8c2
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-05-16 21:06:53 +1000

    similar problem: "001. Two Sum" 《hashmap》

--- Solutions/560_Subarray Sum Equals K.py
+++ Solutions/560_Subarray Sum Equals K.py
@@ -1,5 +1,11 @@
+#!usr/bin/env python3
+# -*- coding:utf-8 -*-
 '''
 560. Subarray Sum Equals K
+
+https://leetcode.com/problems/subarray-sum-equals-k/
+similar problem: "001. Two Sum"
+https://leetcode.com/problems/two-sum/
 
 Given an array of integers and an integer k, you need to find the total number
 of continuous subarrays whose sum equals to k.
@@ -16,22 +22,40 @@ the range of the integer k is [-1e7, 1e7].
 '''
 
 
+class Solution:
+    def subarraySum(self, nums, k):
+        '''
+
+        :param nums: List[int]
+        :param k: int
+        :return: int
+        '''
+        from collections import defaultdict
+        prefix = defaultdict(int)
+        prefix[0] = 1
+        count = 0
+        cur_sum = 0
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            prefix[cur_sum] += 1 #hashmap<cur_sum, occur times>
+            if cur_sum - k in prefix:
+                count += prefix[cur_sum - k]
+
+        return count
+
+print(Solution().subarraySum([1,1,1], 2))
+
 '''
-- solution（1）： count of prefix sum 减少重复计算
+思路：
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
 
-Using a hashtable to store # of a prefix sum occurs so far
-Total sum: sum = nums[0]+...+nums[i]
-if:
-nums[0]+ ... + nums[j] = sum - K
-there exits:
-K = nums[j+1] + ... + nums[n]
+count of prefix sum 减少重复计算
 
-- solution(2): utilizing the prefixsum array of original input:
-
-prefixSum[x] = sum of subarray (0, x): nums[0]+ .. + nums[x]
-
+prefixSum[x] = nums[0]+ .. + nums[x]
 prefixSum[x] = prefixSum[x-1] + nums[x]
- 
+
 sum of subarray (i, j) = prefixSum[j] - prefixSum[i-1]
 
 as: prefixSum[j] = nums[0] + ... + nums[i-1] + nums[i]+ ... + nums[j]
@@ -44,15 +68,15 @@ nums    =    [1, 1, 1]
 index    =   [0, 1, 2]
 prefixSum=[0, 1, 2, 3]
 hashmap = [<0,1><1,1><2,1><3,1>]
-count   =  0 + 0 + 1 + 1               (prefixSum[j] - prefixSum[i] == K)
-问题转化成： find how many pairs <i, j>
-where i < j, prefixSum[j] - prefixSum[i] == K
-Using a HashMap<integer, Integer> to record:
+Using a HashMap<key, value> to record:
 key = prefixSum value
 value = numbers of occurrence of the prefixSum value 
-  
+count   =  0 + 0 + 1 + 1               (prefixSum[j] - K == prefixSum=[0, 1, 2, 3])
+          -2， -1， 0， 1
+问题转化成： find how many pairs <i, j>
+where i < j, prefixSum[j] - prefixSum[i] == K
 '''
-from collections import defaultdict
+
 '''
 defaultdict(function_factory): 构建一个是类似factory的对象，其中key的值，自行赋值，
 但value的类型，是function——factory的类实例，且有默认值
@@ -65,26 +89,4 @@ for k in s:
     d[k] += 1
 print(list(d.items())) 
 >>[('m', 1), ('i', 4), ('s', 4), ('p', 2)]
-
 '''
-class Solution:
-    def subarraySum(self, nums, k):
-        '''
-
-        :param nums: List[int]
-        :param k: int
-        :return: int
-        '''
-
-        prefix = defaultdict(int)
-        prefix[0] = 1
-        count = 0
-        cur_sum = 0
-        for i in range(len(nums)):
-            cur_sum += nums[i]  # increment current sum
-            if cur_sum - k in prefix:
-                count += prefix[cur_sum - k] # ???
-            prefix[cur_sum] += 1
-        return count
-
-print(Solution().subarraySum([1,1,1], 2))
