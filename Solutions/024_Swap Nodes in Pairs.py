953375df1ae683eac8d3a6e2768b87497223dbb7
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-05-17 20:56:07 +1000

    similar problem: "445. Add Two Numbers II"

--- Solutions/024_Swap Nodes in Pairs.py
+++ Solutions/024_Swap Nodes in Pairs.py
@@ -1,6 +1,8 @@
 '''
 24. Swap Nodes in Pairs
 
+https://leetcode.com/problems/swap-nodes-in-pairs/discuss/171788/Python-or-Dummynode-tm
+
 Given a linked list, swap every two adjacent nodes and return its head.
 
 You may not modify the values in the list's nodes, only nodes itself may be changed.
@@ -10,8 +12,6 @@ You may not modify the values in the list's nodes, only nodes itself may be chan
 Example:
 
 Given 1->2->3->4, you should return the list as 2->1->4->3.
-
-reference: https://leetcode.com/problems/swap-nodes-in-pairs/discuss/171788/Python-or-Dummynode-tm
 
 '''
 
