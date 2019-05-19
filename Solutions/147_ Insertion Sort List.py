d8600c0c61bc4479f13d140b73852e5ebe876c76
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-05-19 21:43:21 +1000

    tbc

---
+++ Solutions/147_ Insertion Sort List.py
@@ -0,0 +1,18 @@
+'''
+147. Insertion Sort List
+
+https://leetcode.com/problems/insertion-sort-list/
+similar problem: "21. Merge Two Sorted Lists"
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+Algorithm of Insertion Sort:
+
+Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list.
+At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there.
+It repeats until no input elements remain.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+'''
