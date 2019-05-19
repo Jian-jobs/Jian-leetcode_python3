ef0c4c26ea2c51413b587779627729f2e96a81ae
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-05-19 17:41:47 +1000

    similar problem: "142. Linked List Cycle II"

--- Solutions/141_ Linked List Cycle.py
+++ Solutions/141_ Linked List Cycle.py
@@ -1,6 +1,10 @@
 '''
 141. Linked List Cycle
 
+https://leetcode.com/problems/linked-list-cycle/
+similar problem: "142. Linked List Cycle II"
+https://leetcode.com/problems/linked-list-cycle-ii/
+
 Given a linked list, determine if it has a cycle in it.
 
 To represent a cycle in the given linked list, we use an integer
@@ -16,17 +20,9 @@ Input: head = [3,2,0,-4], pos = 1
 Output: true
 Explanation: There is a cycle in the linked list,
 where tail connects to the second node.
-'''
-'''
-基于上面的想法，Floyd用两个指针，一个慢指针（龟）每次前进一步，
-快指针（兔）指针每次前进两步（两步或多步效果时等价的，只要一个比另一个快就行）。
-如果两者在链表头以外的某一点相遇（即相等）了，那么说明链表有环，
-否则，如果（快指针）到达了链表的结尾，那么说明没坏。
 
-'is' and '==':
-当变量是数字、字符串、元组，列表，字典时，is和==都不相同， 
-不能互换使用！当比较值时，要使用==，比较是否是同一个内存地址时应该使用is。
 '''
+
 
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -40,7 +36,7 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        fast = slow = head
+        fast = slow = head # or fast, slow = head, head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
@@ -48,3 +44,16 @@ class Solution(object):
                 return True
 
         return False
+'''
+思路：
+基于上面的想法，Floyd用两个指针，一个慢指针（龟）每次前进一步，
+快指针（兔）指针每次前进两步（两步或多步效果时等价的，只要一个比另一个快就行）。
+如果两者在链表头以外的某一点相遇（即相等）了，那么说明链表有环，
+否则，如果（快指针）到达了链表的结尾，那么说明没环。
+
+'is' and '==':
+当变量是数字、字符串、元组，列表，字典时，is和==都不相同， 不能互换使用！
+当比较值时，要使用==，比较是否是同一个内存地址时应该使用is。
+
+'''
+
