d99e337474a692e3dc6842c47e0e75073628a706
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-05-19 19:18:45 +1000

    similar problem: "23. Merge k Sorted Lists"

--- Solutions/021_Merge Two Sorted Lists.py
+++ Solutions/021_Merge Two Sorted Lists.py
@@ -1,6 +1,10 @@
 '''
 21. Merge Two Sorted Lists
 
+https://leetcode.com/problems/two-sum/
+similar problem: "23. Merge k Sorted Lists"
+https://leetcode.com/problems/merge-k-sorted-lists/
+
 Merge two sorted linked lists and return it as a new list.
 The new list should be made by splicing together the nodes of the first two lists.
 
