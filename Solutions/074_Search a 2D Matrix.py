6c23f46c726d8d30584e1e7bc2af5fddd1b41739
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2019-07-01 13:16:21 +1000

    treat 2d as 1d "binary search"

--- Solutions/074_Search a 2D Matrix.py
+++ Solutions/074_Search a 2D Matrix.py
@@ -31,3 +31,31 @@ target = 13
 Output: false
 
 '''
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i = len(matrix) - 1
+        j = 0
+        while i > -1 and j < len(matrix[0]):
+            if target == matrix[i][j]:
+                return True
+            elif target > matrix[i][j]:
+                j = j + 1
+            else:
+                i = i - 1
+        return False
+
+
+# reference:
+# https://leetcode.com/problems/search-a-2d-matrix/discuss/289393/Beat-100-python-time-max-o(n)space-o(1)
+'''
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+len(matrix) row
+3
+len(matrix[0]) col
+4
+'''
