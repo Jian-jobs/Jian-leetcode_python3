38175045ee34499c110f4e11d86d07f976cffa8c
Jian-Chueng <42600298+Jian-Chueng@users.noreply.github.com>
2020-04-25 20:26:46 +1000

    可以直接测试结果

--- Solutions/704_Binary Search.py
+++ Solutions/704_Binary Search.py
@@ -29,28 +29,36 @@ The value of each element in nums will be in the range [-9999, 9999].
 '''
 
 
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-            for i in range(len(nums)):
-                if nums[i] == target:
-                    return i
-            else:
-                return -1
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#             for i in range(len(nums)):
+#                 if nums[i] == target:
+#                     return i
+#             else:
+#                 return -1
 
 # 自己写的
 
 # reference：二叉树
-class Solution:
-    def search(self, nums, target):
-        left,right = 0, len(nums)-1
-        while left <= right:
-            mid = (left+right) // 2
-            #// 取整除 - 返回商的整数部分（向下取整）
-            #print(left,right,mid)
-            if nums[mid] < target:
-                left = mid+1
-            elif nums[mid] > target:
-                right = mid-1
-            else:
-                return mid
-        return -1
+# class Solution:
+def search(nums, target):
+    left,right = 0, len(nums)-1
+    while left <= right:
+        mid = (left+right) // 2
+        #// 取整除 - 返回商的整数部分（向下取整）
+        #print(left,right,mid)
+        if nums[mid] < target:
+            left = mid+1
+        elif nums[mid] > target:
+            right = mid-1
+        else:
+            return mid
+    return -1
+
+
+
+if __name__ == "__main__":
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 2
+    print(search(nums, target))
+
